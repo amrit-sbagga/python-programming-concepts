0b2bd66700eb1b90d67df8c95d5276b61eb972d2
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-14 17:13:20 +0530

    Added some inbuilt functions

---
+++ myfunctions.py
@@ -0,0 +1,25 @@
+print(str(5))
+
+print(str(5.5))
+
+print(str(True))
+
+print(int("5"))
+
+print(float("6.7"))
+
+print(bool("True"))
+
+print(len("Hello"))
+
+arrLength = print(len([4,5,6]))
+
+print(sorted([2, 3, 76, 43, 65, 12, 13, 66, 99, 53]))
+
+# sorted with numbers then caps then small letters
+print(sorted(["B", "R", "N", "F", "a", "5", "d", "3.5", "2"]))
+
+
+
+
+
