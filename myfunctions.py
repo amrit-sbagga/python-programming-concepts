31e9e35becc542af279ad9717d7ebd166a0ca102
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-14 17:23:48 +0530

    Added user defined functions with argumants

--- myfunctions.py
+++ myfunctions.py
@@ -19,7 +19,13 @@ print(sorted([2, 3, 76, 43, 65, 12, 13, 66, 99, 53]))
 # sorted with numbers then caps then small letters
 print(sorted(["B", "R", "N", "F", "a", "5", "d", "3.5", "2"]))
 
+#use snake case for function names
+def my_function(str1, str2, str3):
+    print("This is my function")
+    print("second string inside this function , str1 = " + str(str1))
+    print(str2)
+    print(str3)
 
-
+my_function(5555, 66, "this is arg3")
 
 
