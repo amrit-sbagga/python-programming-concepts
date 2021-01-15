74f55b0e5a08b52437f458ae7d2d9c364625509a
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-15 16:51:44 +0530

    Advance concepts usage - white space, indentation

---
+++ advance-concepts.py
@@ -0,0 +1,26 @@
+import os
+import sys
+#from MyModule import include foo, bar, foobar
+
+def my_function(one, two,
+                three, four,
+                five, six):
+    print("Hello..!!")
+
+# 2 white lines between 2 different function
+def second_function():
+    print("Second function")
+
+
+my_list = [1, 2,
+           3, 4,
+           5, 6]
+print(my_list[2])
+
+#usage of white space
+x = 3*52 + 7*2
+
+check = True
+
+if check is True:
+    print("This is true")
