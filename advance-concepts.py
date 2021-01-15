25a3ad85128276e3732007f22fb47fc746d0c82b
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-15 17:50:35 +0530

    Advance concepts - break, continue, pass

--- advance-concepts.py
+++ advance-concepts.py
@@ -15,12 +15,44 @@ def second_function():
 my_list = [1, 2,
            3, 4,
            5, 6]
-print(my_list[2])
+#print(my_list[2])
 
 #usage of white space
 x = 3*52 + 7*2
 
 check = True
 
-if check is True:
-    print("This is true")
+#if check is True:
+#    print("This is true")
+
+import random
+
+playerhp = 260
+enemyattacklow = 60
+enemtattackhigh = 80
+
+# usage for breaking while loop
+while playerhp > 0:
+    dmg = random.randrange(enemyattacklow, enemtattackhigh)
+    playerhp = playerhp - dmg
+
+    if playerhp <= 30:
+        playerhp = 30
+        break
+
+    # for empty lock of code use pass
+    #if playerhp <= 30:
+        #pass
+
+    print("Enemy strikes for ", dmg, "points of damage. Current HP is", playerhp)
+
+    if playerhp == 30:
+        print("You have low health...!!")
+
+    # usage of continue
+    if playerhp > 30:
+        continue
+
+
+
+
